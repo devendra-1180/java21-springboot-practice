5ffeef8b052416f1950063ce2132ab4acf7630aa
Devendra Singh Tomar <devendra.tomar@intelegencia.com>
2025-10-18 19:33:14 +0530

    TDD for service layer, added other service methods and tests

--- src/main/java/com/learning/tdd/service/TaskService.java
+++ src/main/java/com/learning/tdd/service/TaskService.java
@@ -3,12 +3,22 @@ package com.learning.tdd.service;
 import com.learning.tdd.model.Task;
 import com.learning.tdd.repository.TaskRepository;
 
+import java.util.List;
+
 public record TaskService(TaskRepository taskRepository) {
 
     public Task updateTaskStatus(Long Id, String taskStatus) {
-        Task taskToUpdate = taskRepository.findById(Id).orElseThrow(() -> new IllegalArgumentException("Task not found"));
+        final Task taskToUpdate = getTaskById(Id);
         taskToUpdate.setStatus(taskStatus);
         return taskRepository.save(taskToUpdate);
+    }
+
+    public Task getTaskById(Long Id) {
+        return taskRepository.findById(Id).orElseThrow(() -> new IllegalArgumentException("Task not found"));
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
     }
 }
 
--- src/test/java/com/learning/tdd/service/TaskServiceTest.java
+++ src/test/java/com/learning/tdd/service/TaskServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +23,50 @@ public class TaskServiceTest {
 
     @InjectMocks
     TaskService taskService;
+
+    @Test
+    void testGetTaskById() {
+        //arrange
+        Task task =  new Task(1L, "New Task", "In  Progress");
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        //act & assert
+        assertEquals(task, taskService.getTaskById(1L));
+
+        verify(taskRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testGetAllTask(){
+        //arrange
+        List<Task> tasks = Arrays.asList(
+                new Task(1L, "Task 1", "To do"),
+                new Task(2L, "Task 2", "In progress"),
+                new Task(3L, "Task 3", "Done")
+        );
+        when(taskRepository.findAll()).thenReturn(tasks);
+
+        //act
+        List<Task> resultTask = taskService.getAllTasks();
+
+        //assert
+        assertNotNull(resultTask);
+        assertEquals(tasks.size(), resultTask.size());
+        assertEquals(tasks.getFirst().getId(), resultTask.getFirst().getId());
+
+        verify(taskRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetTaskById_TaskNotFound(){
+        //arrange
+        when(taskRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        //act & assert
+        assertThrowsExactly(IllegalArgumentException.class, () -> taskService.getTaskById(1L));
+        verify(taskRepository, times(1)).findById(1L);
+    }
+
 
     @Test
     void testUpdateTask(){
@@ -39,6 +85,6 @@ public class TaskServiceTest {
 
         verify(taskRepository, times(1)).findById(1L);
         verify(taskRepository, times(1)).save(any(Task.class));
-
     }
+
 }
